ff3f89abb64d235004ef6942c2844dc0d5aa8a2e
andrejskalin966-cpu <andrejskalin966@gmail.com>
2026-07-25 20:49:52 +0300

    Update equations_system.hpp

--- equations_system.hpp
+++ equations_system.hpp
@@ -1,52 +1,53 @@
 #ifndef EQUATIONS_SYSTEM
 #define EQUATIONS_SYSTEM
 
-#include <iostream> //console (in/out) stream
-#include <cstdint>  //std::uint8_t
-#include <string>   //std::string
-#include <vector>   //std::vector
+#include <iostream>  //console (in/out) stream
+#include <cstdint>   //std::uint8_t
+#include <string>    //std::string
+#include <vector>    //std::vector
+#include <algorithm> // для swap
 
-class literal
-{
-private:
-    int id;     // Идентификационный номер литерала равный 2i для x_i и 2i+1 для NOT(x_i).
-    char value; // Найденное в ходе решения значение.
+class literal {
+    private:
+        int id;             //Идентификационный номер литерала равный 2i для x_i и 2i+1 для NOT(x_i).
+        std::uint8_t value; //Найденное в ходе решения значение.
 
-    literal();
-    literal(int id);
-    literal(int id, char value);
+        literal();
+        literal(int id0);
+        literal(int id0, std::uint8_t value0);
 
-    ~literal();
+        ~literal();
 
-    void set_value(char value);
+    public:
+        literal* create();
+        literal* create(int id0);
+        literal* create(int id0, std::uint8_t value0);
+        //Функции выше предназначены для корректного создания создания объекта литерала.
 
-    char get_value() const;
+        void destroy();
+        //Функция предназначена для корректного удаления объекта литерала.
 
-public:
-    literal *create();
-    literal *create(int id);
-    literal *create(int id, char value);
-    // Функции выше предназначены для корректного создания создания объекта литерала.
+        void set_value(std::uint8_t value0);
 
-    void destroy();
-    // Функция предназначена для корректного удаления объекта литерала.
+        std::uint8_t get_value() const;
 
-    bool is_known();
-    // Функция предназаначена для проверки заданныли значения литерала.
-    // Возвращает 0 если значеничение литерала уже было инициализровано ранее.
-    // Возвращает 1 в противном случае.
+        bool is_known();
+        //Функция предназаначена для проверки заданныли значения литерала.
+        //Возвращает 1 если значеничение литерала уже было инициализровано ранее.
+        //Возвращает 0 в противном случае.
 };
 
 class equation
 {
 private:
     bool Is_line;                      // В рамках данной работы уравнения могут быть только линейными или квадратичные.
-    std::vector<literal *> param_form; // Задает порядок литералов в уравнении.
-    /*Переменные выше в рамках данной работы одназначно определяют любое возможное уравнение.
+    std::vector<literal*> param_form;  // Задает порядок литералов в уравнении.
+    /*
+    Переменные выше в рамках данной работы одназначно определяют любое возможное уравнение.
 
     Пример 1:
 
-        Is_line = 0 (линейное)
+        Is_line = 1 (линейное)
         param_form = {
             -> id=2
             -> id=3
@@ -57,7 +58,7 @@ private:
 
     Пример 2:
 
-        Is_line = 1 (Квадратичное)
+        Is_line = 0 (Квадратичное)
         param_form = {
             -> id=2
             -> id=4
@@ -68,35 +69,35 @@ private:
     */
 
     equation();
-    equation(param_form);
-    equation(Is_line, param_form);
+    equation(Is_line0);
+    equation(param_form0);
+    equation(Is_line0, param_form0);
 
     ~equation();
 
-    void set_Is_line(bool Is_line);
-    void set_param_form(std::vector<literal> param_form);
-
-    bool get_Is_line() const;
-    std::vector<literal> get_param_form() const;
-
 public:
-    equation *create(param_form);
-    equation *create(Is_line, param_form);
-    // Функции выше предназначены для корректного создания создания объекта уравнения.
+    equation* create(Is_line0, param_form0);
+    // Функция выше предназначена для корректного создания создания объекта уравнения.
 
     void destroy();
     // Функция предназначена для корректного удаления объекта уравнения.
 
+    void set_Is_line(bool Is_line0);
+    void set_param_form(std::vector<literal*> param_form0);
+
+    bool get_Is_line() const;
+    std::vector<literal*> get_param_form() const;
+
     bool is_linear();
     // Эта функция предназначена для решения системы (и уравнений в частности), реализации логики графа.
-    // Вернет 0 в случае если уравнение линейно.
-    // Вернет 1 в противном случае.
+    // Вернет 1 в случае если уравнение линейно.
+    // Вернет 0 в противном случае.
 };
 
 class MQS_system
 {
 private:
-    std::vector<literal *> param_num; // Массив указателей литералы используемые системы (и сопряженые им)
+    std::vector<literal*> param_num;  // Массив указателей литералы используемые системы (и сопряженые им)
     std::vector<equation> equations;  // Массив уравнений системы
 
     MQS_system();
@@ -104,19 +105,19 @@ private:
 
     ~MQS_system();
 
+public:
+    MQS_system* create(std::string filepath);
+    MQS_system* create(std::vector<equation> equations);
+    // Функции выше предназначены для корректного создания объекта системы.
+
+    void destroy();
+    // Эта функция предназначена для корректного удаления объекта системы.
+
     void set_param_num(std::vector<literal> param_num);
     void set_equations(std::vector<equation> equations);
 
     std::vector<literal> get_param_num() const;
     std::vector<equation> get_equations() const;
-
-public:
-    MQS_system *create(std::string filepath);
-    MQS_system *create(std::vector<equation> equations);
-    // Функции выше предназначены для корректного создания объекта системы.
-
-    void destroy();
-    // Эта функция предназначена для корректного удаления объекта системы.
 
     MQS_system unit_propagation();
     // Эта функция возвращает объект системы сокращенный с использоавнием имеющихся данных.
@@ -127,8 +128,8 @@ public:
 
     bool is_linear();
     // Эта функция предназначена для решения системы (и уравнений в частности), реализации логики графа.
-    // Вернет 0 в случае если система линейна.
-    // Вернет 1 в противном случае.
+    // Вернет 1 в случае если система линейна.
+    // Вернет 0 в противном случае.
 
     bool add_literal_value();
     // Эта функция предназначена для вызова из части программы отвечающей за решение уравнений.
