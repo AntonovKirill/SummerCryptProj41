c25c283190b9adc8d3ba5b9f1d462b33a84e775a
cloudy-crypto <ponomareva.barbara@gmail.com>
2026-07-27 18:06:40 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,7 @@
-/*#include <iostream>
-
-int main(int argc, char* argv[]) {
-    // Проверяем количество переданных аргументов
-    if (argc != 4) {  // argc включает имя программы + 3 параметра
-        std::cerr << "Error: too little param!" << std::endl;
-        std::cerr << "Example: " << argv[0] 
-                  << " <param1> <param2> <param3>" << std::endl;
-        return 1;
-    }
-    return 0;
-}*/
-
 #include <iostream>
 #include <string>
 #include <fstream>
+#include "search_tree.hpp"
 
 int main(int argc, char* argv[])
 {
@@ -149,7 +137,19 @@ int main(int argc, char* argv[])
     }
 
     std::cout << std::endl;
-    std::cout << "Ready! (solving not implemented yet)" << std::endl;
+
+    std::cout << "=== STARTING SEARCH ===" << std::endl;
+
+    SearchTree tree(vars);
+
+    if (tree.solve())
+    {
+        tree.printSolution();
+    }
+    else
+    {
+        std::cout << "UNSAT: No solution found!" << std::endl;
+    }
 
     return 0;
 }
