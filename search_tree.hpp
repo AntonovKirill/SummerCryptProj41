5e5330b742a8d29a76737fcfbb164fbb78a84bc8
cloudy-crypto <ponomareva.barbara@gmail.com>
2026-07-30 11:57:34 +0300

    Update search_tree.hpp

--- search_tree.hpp
+++ search_tree.hpp
@@ -2,37 +2,42 @@
 #define SEARCH_TREE_HPP
 
 #include <vector>
+#include <stack>
+#include <utility>
 #include "equations_system.hpp"
 
 class SearchNode
 {
 public:
-    int num;           
-    int val;            
-    SearchNode* left;   
-    SearchNode* right;  
-    SearchNode* par;   
+    int num;                     
+    int val;                     
+    SearchNode* left;
+    SearchNode* right;
+    SearchNode* par;
 
-    
     SearchNode(int n, int v, SearchNode* p = nullptr);
+    ~SearchNode();
 };
 
 class SearchTree
 {
 private:
-    SearchNode* root;          
-    int count;                  
-    std::vector<int> assign;    
-    int steps;                  
-    MQS_system* system;         
+    SearchNode* root;
+    int varCount;
+    int steps;
+    MQS_system* system;
 
-    
+    std::stack< std::pair< std::vector<literal>, std::vector<equation> > > stateStack;
+
     bool unitPropagation();
     bool checkEquation(const equation& eq);
+    void saveState();
+    void restoreState();
+    void applyState(const std::vector<literal>& params, const std::vector<equation>& equations);
 
 public:
-    
     SearchTree(MQS_system& sys);
+    ~SearchTree();
 
     bool solve();
     bool backtrack(SearchNode* node);
